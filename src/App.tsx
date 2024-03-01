9cf2a720083bafbdbfe20cec22ff4f68fbf8190c
Loc Nguyen <nguyentanloc@teams.airlab.dev>
2024-03-01 16:55:52 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import Button from "@mui/material/Button";
 
 function App() {
-
   return (
     <>
-
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
